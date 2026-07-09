dd06031c00f797f1b3af904736ffb6a59be9b0c2
Aman <amanmaurya917@gmail.com>
2026-07-09 13:05:41 +0530

    updating supabasejs again

--- dashboard-frontend/src/lib/supabase.js
+++ dashboard-frontend/src/lib/supabase.js
@@ -2,13 +2,15 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 console.log({
   url: supabaseUrl,
-  key: supabaseAnonKey?.slice(0,10)
+  key: supabaseAnonKey?.slice(0,10),
+  publishkey: supabasePublishableKey?.slice(0,10)
 });
 
 export const supabase = createClient(
   supabaseUrl,
-  supabaseAnonKey
+  supabasePublishableKey
 );
